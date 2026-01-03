5a27966906443f4fc0789dc23e30d8008e46f938
mzubair12147 <muhammadzubair12147@gmail.com>
2026-01-03 22:04:33 +0500

    Remove Powered by Noxionite footer and reduce mobile spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,10 @@
 import cs from 'classnames'
-import Image from 'next/image'
 import * as React from 'react'
 import styles from 'styles/components/common.module.css'
 
 import * as config from '@/lib/config'
 
 import { PageSocial } from './PageSocial'
-import ShinyText from './react-bits/ShinyText'
 
 export function FooterImpl({ isMobile }: { isMobile: boolean }) {
   const currentYear = new Date().getFullYear()
@@ -38,38 +36,6 @@ export function FooterImpl({ isMobile }: { isMobile: boolean }) {
             <div style={{ order: isMobile ? 1 : 2 }}>
               <PageSocial />
             </div>
-          </div>
-
-          <div style={{
-            textAlign: 'center',
-            marginTop: '1rem',
-            fontSize: '0.875rem',
-            order: 3
-          }}>
-            <a
-              href="https://noxionite.vercel.app"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{
-                display: 'inline-flex',
-                alignItems: 'center',
-                textDecoration: 'none',
-                color: 'inherit',
-                cursor: 'pointer'
-              }}
-            >
-              <span>Powered by</span>
-              <Image
-                src="/Noxionite-icon.png"
-                alt="Noxionite"
-                width={16}
-                height={16}
-                style={{
-                  margin: '0 4px 0 8px'
-                }}
-              />
-              <ShinyText text="Noxionite" speed={3} />
-            </a>
           </div>
         </div>
       </div>
